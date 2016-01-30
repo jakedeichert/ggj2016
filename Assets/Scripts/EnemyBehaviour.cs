2a31df749a4c4cb1f5b1130898744c602088cdba
Jake Deichert <jakedeichert@users.noreply.github.com>
2016-01-30 01:37:29 -0500

    Add simple enemy movement that targets the player

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+    public Transform playerTarget;
+    Vector3 movement;
+
+
+	void Start() {
+        Debug.Log(playerTarget.transform.position.y);
+	}
+	
+	void Update () {
+        float distance = Vector3.Distance(playerTarget.transform.position, transform.position);
+
+        if (distance < 10) {
+            Debug.Log("close" + distance);
+
+            float speed = 3.0f;
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, playerTarget.transform.position, step);
+        }
+    }
+}
