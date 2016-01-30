d3026b7e99e6d66a0d1626b7b47c76a523448841
Jake Deichert <jakedeichert@users.noreply.github.com>
2016-01-30 13:20:52 -0500

    Second version of seek AI

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,69 +3,67 @@ using System.Collections;
 
 public class EnemyBehaviour : MonoBehaviour {
     public Transform playerTarget;
-    Vector3 velocity;
+    Vector2 velocity;
+    bool shouldSeek = true;
     float maxVelocity;
     float seekDistance;
     float mass;
 
 
 	void Start() {
-        velocity = Vector3.zero;
-        maxVelocity = 10.0f;
-        seekDistance = 10.0f;
+        velocity = Vector2.zero;
+        maxVelocity = 5.0f;
+        seekDistance = 15.0f;
         mass = 20.0f;
-        Debug.Log(playerTarget.transform.position.y);
 	}
 	
 	void Update () {
         float distance = Vector3.Distance(playerTarget.transform.position, transform.position);
         if (distance < seekDistance && distance > 2) {
             seek(playerTarget.position);
+        } else {
+            velocity = Vector2.zero;
         }
     }
 
     void seek(Vector3 target) {
-        Vector3 dir = target - transform.position;
-        dir.z = 0;
+        Vector2 dir = target - transform.position;
 
-        Vector3 desiredVelocity = dir.normalized * maxVelocity;
-        Vector3 steering = desiredVelocity - velocity;
-        //steering += collisionAvoidance(new Vector3(0, 3, 0), 1.6f, dir);
-        //steering += collisionAvoidance(new Vector3(0, 3, 0) + self.transform.right * 2.5f, 1.3f, dir);
-        //steering += collisionAvoidance(new Vector3(0, 3, 0) - self.transform.right * 2.5f, 1.3f, dir);
+        Vector2 desiredVelocity = dir.normalized * maxVelocity;
+        Vector2 steering = desiredVelocity - velocity;
+        steering += collisionAvoidance(new Vector2(0, 0), 1f, dir);
+        steering += collisionAvoidance(new Vector2(0, 0), 1f, Quaternion.Euler(0,0, 60f) * dir);
+        steering += collisionAvoidance(new Vector2(0, 0), 1f, Quaternion.Euler(0, 0, -60f) * dir);
         float maxForce = 1000.0f;
         steering = truncate(steering, maxForce);
         steering = steering / mass;
 
         velocity = truncate(velocity + steering, maxVelocity); 
-        velocity.z = 0;
-        Debug.Log(velocity.magnitude);
 
         move();
     }
 
-    //Vector3 collisionAvoidance(Vector3 rayOriginOffset, float rayLengthScale, Vector3 dir) {
-    //    Vector3 avoidance = Vector3.zero;
-    //    float rayLength = 10.0f * rayLengthScale;
+    Vector2 collisionAvoidance(Vector2 rayOriginOffset, float rayLength, Vector2 dir) {
+        Vector2 avoidance = Vector2.zero;
 
-    //    if (rayLength > 0) {
-    //        float maxAvoidForce = 120.0f;
-    //        RaycastHit hit;
-    //        Ray ray = new Ray(self.transform.position + rayOriginOffset, dir);
-    //        Debug.DrawRay(ray.origin, ray.direction * rayLength);
+        if (rayLength > 0) {
+            float maxAvoidForce = 3.0f;
+            Ray2D ray = new Ray2D((Vector2)transform.position + rayOriginOffset, dir);
+            Debug.DrawRay(ray.origin, ray.direction * rayLength);
 
-    //        if (Physics.Raycast(ray, out hit, rayLength)) {
-    //            if (hit.collider.name == "tree collider") {
-    //                avoidance.x = ray.origin.x + velocity.x - hit.transform.position.x;
-    //                avoidance.z = ray.origin.z + velocity.z - hit.transform.position.z;
-    //                avoidance.Normalize();
-    //                avoidance *= maxAvoidForce;
-    //            }
-    //        }
-    //    }
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, rayLength);
+            if (hit) {
+                if (hit.collider.name == "skeleton") {
+                    avoidance.x = ray.origin.x + velocity.x - hit.transform.position.x;
+                    avoidance.y = ray.origin.y + velocity.y - hit.transform.position.y;
+                    avoidance.Normalize();
+                    avoidance *= maxAvoidForce;
+                }
+            }
+        }
 
-    //    return avoidance;
-    //}
+        return avoidance;
+    }
 
     Vector3 truncate(Vector3 v, float max) {
         return (v.magnitude > max) ? v.normalized * max : v;
@@ -73,6 +71,6 @@ public class EnemyBehaviour : MonoBehaviour {
 
 
     void move() {
-        transform.position += velocity * Time.deltaTime;
+        transform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
