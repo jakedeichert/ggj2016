9915d11957a721ad89d09e3269a17c1e2f3b1f3b
Coteh <james_c_34@live.com>
2016-01-29 22:34:31 -0500

    Basic movement.

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+    Vector3 movement;
+    public float speed = 10;
+
+	void Start () {
+        movement = new Vector3(0,0,0);
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            movement.x = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            movement.x = speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            movement.y = speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            movement.y = -speed;
+        }
+
+        transform.position += movement * Time.deltaTime;
+        movement.x *= 0.95f;
+        movement.y *= 0.95f;
+	}
+}
