d5e6063009ca277bb441106f271736eed17bb88f
Coteh <james_c_34@live.com>
2016-01-30 13:44:08 -0500

    Add health UI indicator.

---
+++ Assets/Scripts/HealthUIBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HealthUIBehaviour : MonoBehaviour {
+    public HittableBehaviour playerHittable;
+    Text uiText;
+
+    void Start() {
+        uiText = GetComponent<Text>();
+    }
+
+    void Update() {
+        uiText.text = "Health: " + playerHittable.health;
+    }
+}
