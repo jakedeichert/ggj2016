bb63def34858991be19cccef68b65ec14e1b9ce7
Matt Joncas <mattjoncas@users.noreply.github.com>
2016-01-31 16:28:18 -0500

    Added map.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public int gridX = 0, gridY = 0;
+
+    public bool isWall = true;
+    public bool empty = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetGridPos(int _x, int _y) {
+        gridX = _x;
+        gridY = _y;
+    }
+}
