6b8173099175da71eb45220b5747eebaaae7b99b
Coteh <james_c_34@live.com>
2016-01-31 15:57:53 -0500

    Reset level when dead. Add scene fade in and out. Add isDead bool to HittableBehaviour. Fix skeleton attack image.

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,13 +6,19 @@ public class UIController : MonoBehaviour{
     public ItemListUIBehaviour itemListUI;
     public CurrentItemUIBehaviour currItemUI;
 
+    public GameMain gameMain;
+
     void Start() {
 
     }
 
-    public void BringUpItems(bool expression) {
-        if (itemListUI == null) return;
-        itemListUI.gameObject.SetActive(expression);
-        itemListUI.Populate();
+    //public void BringUpItems(bool expression) {
+    //    if (itemListUI == null) return;
+    //    itemListUI.gameObject.SetActive(expression);
+    //    itemListUI.Populate();
+    //}
+
+    void ResetLevel() {
+        gameMain.SendMessage("RestartLevel");
     }
 }
