f42d8992025b35b1568aba985c6d55ff7ff8cf57
Coteh <james_c_34@live.com>
2016-01-31 19:43:57 -0500

    Shrink box collider down to player's feet. Set isDead to true when player is dead. Only update player movement when they're alive.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -29,89 +29,91 @@ public class PlayerBehaviour : MonoBehaviour {
 	}
 	
 	void Update () {
-        if (Input.GetKey(KeyCode.A)) {
-            movement.x = -speed;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            movement.x = speed;
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            movement.y = speed;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            movement.y = -speed;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            if (inventory != null) {
-                inventory.UseItemByIndex(0);
+        if (hittable == null || !hittable.IsDead) {
+            if (Input.GetKey(KeyCode.A)) {
+                movement.x = -speed;
             }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            if (inventory != null) {
-                inventory.UseItemByIndex(1);
+            if (Input.GetKey(KeyCode.D)) {
+                movement.x = speed;
             }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            if (inventory != null) {
-                inventory.UseItemByIndex(2);
+            if (Input.GetKey(KeyCode.W)) {
+                movement.y = speed;
             }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4)) {
-            if (inventory != null) {
-                inventory.UseItemByIndex(3);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5)) {
-            if (inventory != null) {
-                inventory.UseItemByIndex(4);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha6)) {
-            if (inventory != null) {
-                inventory.UseItemByIndex(5);
-            }
-        }
-
-        if (weapon != null) {
-            if (Input.GetMouseButtonDown(0)) {
-                isChargingWeapon = true;
+            if (Input.GetKey(KeyCode.S)) {
+                movement.y = -speed;
             }
 
-            if (Input.GetMouseButton(0) && isChargingWeapon) {
-                weapon.AddCharge(1.0f * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                if (inventory != null) {
+                    inventory.UseItemByIndex(0);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                if (inventory != null) {
+                    inventory.UseItemByIndex(1);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                if (inventory != null) {
+                    inventory.UseItemByIndex(2);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4)) {
+                if (inventory != null) {
+                    inventory.UseItemByIndex(3);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5)) {
+                if (inventory != null) {
+                    inventory.UseItemByIndex(4);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6)) {
+                if (inventory != null) {
+                    inventory.UseItemByIndex(5);
+                }
             }
 
-            if (Input.GetMouseButtonUp(0) && isChargingWeapon) {
-                weapon.UseWeapon(attackBoost);
-                isChargingWeapon = false;
+            if (weapon != null) {
+                if (Input.GetMouseButtonDown(0)) {
+                    isChargingWeapon = true;
+                }
+
+                if (Input.GetMouseButton(0) && isChargingWeapon) {
+                    weapon.AddCharge(1.0f * Time.deltaTime);
+                }
+
+                if (Input.GetMouseButtonUp(0) && isChargingWeapon) {
+                    weapon.UseWeapon(attackBoost);
+                    isChargingWeapon = false;
+                }
             }
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(10, 0, 0));
+            if (weapon != null) {
+                Vector3 weaponForward = (mousePos - transform.position).normalized;
+                weaponForward.z = 0;
+                WeaponBehaviour weaponBehav = weapon.GetComponent<WeaponBehaviour>();
+                if (weaponBehav != null) {
+                    weaponBehav.Direction = weaponForward;
+                }
+                Vector3 weaponForwardNor = (weaponForward).normalized;
+                float angle = Mathf.Atan2(weaponForwardNor.y, weaponForwardNor.x) * Mathf.Rad2Deg;
+                Quaternion newRotation = new Quaternion();
+                newRotation.eulerAngles = new Vector3(0, 0, angle);
+                weapon.transform.rotation = newRotation;
+                if (newRotation.eulerAngles.z > 90 && newRotation.eulerAngles.z < 270) {
+                    FlipDirection(-1);
+                } else {
+                    FlipDirection(1);
+                }
+            }
+
+            Vector3 newPos = transform.position + movement * Time.deltaTime;
+            rBody.MovePosition(newPos);
+            movement.x *= friction;
+            movement.y *= friction;
         }
-
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(10, 0, 0));
-        if (weapon != null) {
-            Vector3 weaponForward = (mousePos - transform.position).normalized;
-            weaponForward.z = 0;
-            WeaponBehaviour weaponBehav = weapon.GetComponent<WeaponBehaviour>();
-            if (weaponBehav != null) {
-                weaponBehav.Direction = weaponForward;
-            }
-            Vector3 weaponForwardNor = (weaponForward).normalized;
-            float angle = Mathf.Atan2(weaponForwardNor.y, weaponForwardNor.x) * Mathf.Rad2Deg;
-            Quaternion newRotation = new Quaternion();
-            newRotation.eulerAngles = new Vector3(0, 0, angle);
-            weapon.transform.rotation = newRotation;
-            if (newRotation.eulerAngles.z > 90 && newRotation.eulerAngles.z < 270) {
-                FlipDirection(-1);
-            } else {
-                FlipDirection(1);
-            }
-        }
-
-        Vector3 newPos = transform.position + movement * Time.deltaTime;
-        rBody.MovePosition(newPos);
-        movement.x *= friction;
-        movement.y *= friction;
 	}
 
     void FlipDirection(int direction) {
